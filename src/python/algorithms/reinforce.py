53586c1ad9f3c1d9ae853fc2cc8bcfdb1f9a5289
viraj <viraj.ganatra@zohomail.eu>
2024-08-17 21:24:00 +0100

    feat: implement REINFORCE algorithm

---
+++ src/python/algorithms/reinforce.py
@@ -0,0 +1,44 @@
+import math
+from collections import deque
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class REINFORCE(nn.Module):
+
+
+
+    def __init__(self, policy, learning_rate=1e-2, gamma=0.99):
+        super(REINFORCE, self).__init__()
+        self.policy = policy
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=learning_rate)
+        self.gamma = gamma
+
+
+    def update(self, states, actions, rewards):
+        returns = self._compute_returns(rewards)
+        loss = self._compute_loss(states, actions, returns)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def _compute_returns(self, rewards):
+        returns = deque()
+        R = 0 #return of state (after discounting)
+        for r in rewards[::-1]:
+            R = r + self.gamma * R
+            returns.appendleft(R)
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + math.ulp(0.0))
+        return returns
+
+    def _compute_loss(self, states, actions, returns):
+        states = torch.tensor(states, dtype=torch.float)
+        actions = torch.tensor(actions)
+        log_probs = self.policy.log_probs(states, actions)
+        return -(log_probs * returns).sum()
--- src/python/policy.py
+++ src/python/policy.py
@@ -1,4 +1,8 @@
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
 
 class Policy:
     def select_action(self, state):
@@ -10,3 +14,23 @@ class RandomPolicy(Policy):
 
     def select_action(self, state):
         return np.random.choice(self.action_space)
+
+class NNPolicy(nn.Module, Policy):
+    def __init__(self, state_dim, action_dim):
+        super(NNPolicy, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.softmax(x, dim=-1)
+
+    def select_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(state)
+        dist = Categorical(probs)
+        action = dist.sample()
+        return action.item(), dist.log_prob(action)
